87ae600f65c42432ba97d337359bb952691563b3
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-08-02 19:58:34 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,11 @@
+n = int(input('Digite um número:'))
+mult = 0
+for c in range(2, n):
+    if (n % c == 0):
+        print('Múltiplo de', c)
+        mult += 1
+
+if mult == 0:
+     print('É primo')
+else:
+    print('Não é primo')
